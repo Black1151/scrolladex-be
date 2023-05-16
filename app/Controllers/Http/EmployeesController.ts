228115532b96b949203fc28357fd00763b0f4cb3
blackPrawn <danblack9988@gmail.com>
2023-05-16 12:02:46 +0100

    create controllers and routes for departments and employees

---
+++ app/Controllers/Http/EmployeesController.ts
@@ -0,0 +1,38 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Employee from 'App/Models/Employee'
+
+export default class EmployeesController {
+  public async index ({ response }: HttpContextContract) {
+    const employees = await Employee.query().preload('department')
+    return response.json(employees)
+  }
+
+  public async store ({ request, response }: HttpContextContract) {
+    const employeeData = request.only(['title', 'firstName', 'lastName', 'empNo', 'jobTitle', 'departmentId', 'telephone', 'email', 'profilePicture'])
+    const employee = await Employee.create(employeeData)
+    return response.json(employee)
+  }
+
+  public async show ({ params, response }: HttpContextContract) {
+    const employee = await Employee.query().where('id', params.id).preload('department').first()
+    return response.json(employee)
+  }
+
+  public async update ({ params, request, response }: HttpContextContract) {
+    const employeeData = request.only(['title', 'firstName', 'lastName', 'empNo', 'jobTitle', 'departmentId', 'telephone', 'email', 'profilePicture'])
+    const employee = await Employee.find(params.id)
+    if (employee) {
+      employee.merge(employeeData)
+      await employee.save()
+    }
+    return response.json(employee)
+  }
+
+  public async destroy ({ params, response }: HttpContextContract) {
+    const employee = await Employee.find(params.id)
+    if (employee) {
+      await employee.delete()
+    }
+    return response.json(employee)
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.resource('/departments', 'DepartmentsController').apiOnly()
+Route.resource('/employees', 'EmployeesController').apiOnly()
+
