fec82fbd9542387384ed8efb04b08a1640f22403
blackPrawn <danblack9988@gmail.com>
2023-05-25 12:04:31 +0100

    replace email field with username in Users model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -5,9 +5,9 @@ import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
-  
+
   @column()
-  public email: string
+  public username: string
 
   @column({ serializeAs: null })
   public password: string
